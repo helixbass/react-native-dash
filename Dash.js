2e8d1df450ec138668c72203b2b944e55291d14d
Julian Rosse <julian@helixbass.net>
2018-02-06 19:22:44 -0500

    solid prop

--- Dash.js
+++ Dash.js
@@ -10,56 +10,54 @@ import { View, StyleSheet, ViewPropTypes } from 'react-native'
 import MeasureMeHOC from 'react-native-measureme'
 import { getDashStyle, isStyleRow } from './util'
 
-const Dash = (props) => {
-	const isRow = isStyleRow(props.style)
-	const length = isRow ? props.width : props.height
-	const n = Math.ceil(length / (props.dashGap + props.dashLength))
-	const calculatedDashStyles = getDashStyle(props)
-	let dash = []
-	for (let i = 0; i < n; i++) {
-		dash.push(
-			<View
-				key={ i }
-				style={ [
-					calculatedDashStyles,
-					props.dashStyle,
-				] }
-			/>
-		)
-	}
-	return (
-		<View
-			onLayout={ props.onLayout }
-			style={ [ props.style, isRow ? styles.dashRow : styles.dashColumn ] }
-		>
-			{ dash }
-		</View>
-	)
+const Dash = origProps => {
+  const props = { ...origProps }
+  const isRow = isStyleRow(props.style)
+  const length = isRow ? props.width : props.height
+  if (props.solid) {
+    props.dashGap = 0
+    props.dashLength = length
+  }
+  const n = Math.ceil(length / (props.dashGap + props.dashLength))
+  const calculatedDashStyles = getDashStyle(props)
+  let dash = []
+  for (let i = 0; i < n; i++) {
+    dash.push(<View key={i} style={[calculatedDashStyles, props.dashStyle]} />)
+  }
+  return (
+    <View
+      onLayout={props.onLayout}
+      style={[props.style, isRow ? styles.dashRow : styles.dashColumn]}
+    >
+      {dash}
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-	dashRow: {
-		flexDirection: 'row',
-	},
-	dashColumn: {
-		flexDirection: 'column',
-	},
+  dashRow: {
+    flexDirection: 'row',
+  },
+  dashColumn: {
+    flexDirection: 'column',
+  },
 })
 
 Dash.propTypes = {
-	style: ViewPropTypes.style,
-	dashGap: PropTypes.number.isRequired,
-	dashLength: PropTypes.number.isRequired,
-	dashThickness: PropTypes.number.isRequired,
-	dashColor: PropTypes.string,
-	dashStyle: ViewPropTypes.style,
+  style: ViewPropTypes.style,
+  dashGap: PropTypes.number.isRequired,
+  dashLength: PropTypes.number.isRequired,
+  dashThickness: PropTypes.number.isRequired,
+  dashColor: PropTypes.string,
+  dashStyle: ViewPropTypes.style,
+  solid: PropTypes.bool,
 }
 
 Dash.defaultProps = {
-	dashGap: 2,
-	dashLength: 4,
-	dashThickness: 2,
-	dashColor: 'black',
+  dashGap: 2,
+  dashLength: 4,
+  dashThickness: 2,
+  dashColor: 'black',
 }
 
 module.exports = MeasureMeHOC(Dash)
